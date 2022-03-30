06579acce3d30d2e5c7fc1309a16d8e7c5bf0d85
Kevin Chee <kevin@craftypenguins.net>
2022-03-30 14:59:32 -0700

    Add variable

--- prod-to-stage-envs.sh
+++ prod-to-stage-envs.sh
@@ -3,6 +3,7 @@
 touch reseed-direction/source_prefix
 echo staging. > reseed-direction/target_prefix
 echo ${MYSQLUSER} > reseed-direction/source_mysql_user
+echo ${MYSQLUSER} > reseed-direction/target_mysql_user
 echo ${SOURCEMYSQLPASS} > reseed-direction/source_mysql_pass
 echo ${TARGETMYSQLPASS} > reseed-direction/target_mysql_pass
 echo ${SOURCEIP} > reseed-direction/source_ip
