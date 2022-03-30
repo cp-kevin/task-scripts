7f2ed5e3866f70590ffc0c13caa68e4b6b0be6ac
Kevin Chee <kevin@craftypenguins.net>
2022-03-30 14:48:24 -0700

    Increase verbosity

--- reseed-target-database.sh
+++ reseed-target-database.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export TARGETIP=$(cat reseed-direction/target_ip)
-rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
+rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progress target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'DROP DATABASE ${DBNAME}'"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'CREATE DATABASE ${DBNAME}'"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME} < ${SITEDIR}/prod_tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
