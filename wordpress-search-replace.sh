64369ecb1aaeab7a7d10d61c12a114c72db8a616
Kevin Chee <kevin@craftypenguins.net>
2022-03-30 15:36:35 -0700

    Enhance verbosity, generalization

--- wordpress-search-replace.sh
+++ wordpress-search-replace.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -v
 
 export TARGETIP=$(cat reseed-direction/target_ip)
 export SOURCEURL=$(cat site-urls/source_site_url)
@@ -9,5 +10,5 @@ ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} search-replace https://${SOURCEURL} https://${TARGETURL}"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} search-replace http://${SOURCEURL} http://${TARGETURL}"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} --skip-themes --skip-plugins option delete aadsso_settings"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "echo ((online_torontofilmschool_ca.STAGE-AADSSO)) | base64 -d > stage-aadsso"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} --skip-themes --skip-plugins option add aadsso_settings < stage-aadsso; rm stage-aadsso"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "echo ${TARGETAADSSO} | base64 -d > target-aadsso"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} --skip-themes --skip-plugins option add aadsso_settings < target-aadsso; rm target-aadsso"
