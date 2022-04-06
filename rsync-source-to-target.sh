b41766e2d763b6379e776660902473fb145dd4bf
Kevin Chee <kevin@craftypenguins.net>
2022-04-06 15:10:10 -0700

    Remove old comments

--- rsync-source-to-target.sh
+++ rsync-source-to-target.sh
@@ -4,6 +4,3 @@
 export SOURCEIP=$(cat reseed-direction/source_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 export TARGETIP=$(cat reseed-direction/target_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${SOURCEIP} "rsync -e 'ssh -o StrictHostKeyChecking=no -i .ssh/deployer' --rsync-path='sudo rsync' --chown=${WEBUSER}:${WEBUSER} --chmod=D2775,F664 -a --recursive --progress --numeric-ids --delete --omit-dir-times --exclude=wp-config.php ${SITEDIR} deployer@${TARGETIP}:/var/www/"
-
-# rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" -a --recursive --progress --numeric-ids deployer@${SOURCEIP}:${SITEDIR} ./
-# rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --rsync-path="sudo rsync" --chown=${WEBUSER}:${WEBUSER} --chmod=D2775,F664 -a --recursive --progress --numeric-ids --delete --exclude=wp-config.php ${SITE} deployer@${TARGETIP}:/var/www/
