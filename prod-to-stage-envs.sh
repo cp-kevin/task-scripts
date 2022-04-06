f8442e1a7a916a85b5a2d8cace741bb83637fa7a
Kevin Chee <kevin@craftypenguins.net>
2022-04-06 11:15:51 -0700

    Add encryption

--- prod-to-stage-envs.sh
+++ prod-to-stage-envs.sh
@@ -2,10 +2,10 @@
 
 touch reseed-direction/source_prefix
 echo staging. > reseed-direction/target_prefix
-echo ${MYSQLUSER} > reseed-direction/source_mysql_user
-echo ${MYSQLUSER} > reseed-direction/target_mysql_user
-echo ${SOURCEMYSQLPASS} > reseed-direction/source_mysql_pass
-echo ${TARGETMYSQLPASS} > reseed-direction/target_mysql_pass
-echo ${SOURCEIP} > reseed-direction/source_ip
-echo ${TARGETIP} > reseed-direction/target_ip
-echo ${TARGETAADSSO} > reseed-direction/target_aadsso
+echo ${MYSQLUSER} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/source_mysql_user
+echo ${MYSQLUSER} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/target_mysql_user
+echo ${SOURCEMYSQLPASS} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/source_mysql_pass
+echo ${TARGETMYSQLPASS} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/target_mysql_pass
+echo ${SOURCEIP} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/source_ip
+echo ${TARGETIP} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/target_ip
+echo ${TARGETAADSSO} | openssl enc -e -aes-256-ctr -pbkdf2 -a -base64 -salt -pass pass:${ENCRYPTPASS} > reseed-direction/target_aadsso
--- rsync-source-to-target.sh
+++ rsync-source-to-target.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# improve: ssh to target and rsync from there
 export SOURCEIP=$(cat reseed-direction/source_ip)
 export TARGETIP=$(cat reseed-direction/target_ip)
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" -a --recursive --progress --numeric-ids deployer@${SOURCEIP}:${SITEDIR} ./
