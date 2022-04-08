2eee5917d54cf6a9ddf96f364c7a0be629d27c18
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 16:59:05 -0700

    Fix single quote conflict

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -8,4 +8,7 @@ export TARGETIP=$(cat reseed-direction/target_ip | openssl enc -d -aes-256-ctr -
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progress target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
 # ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT} ${WEBROOT}_old"
 # ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT}_new ${WEBROOT}"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE $(grep DB_NAME wp-config.php | cut --delimiter=\' -f4);"; rm /home/deployer/mysql_creds.cnf'
+# Store variable in file because of the delimiter causing issues with the next command
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "echo $(grep DB_NAME wp-config.php | cut --delimiter=\' -f4) > OLDDBNAME"
+# Use of single quotes here is intentional to allow the use of the target's variables
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'export $(cat OLDDBNAME); mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE ${OLDDBNAME};"; rm /home/deployer/OLDDBNAME; rm /home/deployer/mysql_creds.cnf'
