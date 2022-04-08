4acc6596583096b288b8640938948dd5eb69072a
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 15:59:22 -0700

    Change reseed strategy

--- reseed-target-database.sh
+++ reseed-target-database.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 export TARGETIP=$(cat reseed-direction/target_ip  | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
-export YYYMMDDHHMM=$(date +"%Y%m%d%H%M")
+export YYYMMDDHHmm=$(date +"%Y%m%d%H%M")
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progress target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'CREATE DATABASE ${DBNAME}_${YYYMMDDHHMM}'"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME}_${YYYMMDDHHMM} < ${SITEDIR}/prod_tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
-# sed wp-config.php and replace database name
+# User has wildcard grant
+# GRANT ALL PRIVILEGES ON `redacted%`.* TO redacted@'127.0.0.1';
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'CREATE DATABASE ${DBNAME}_${YYYMMDDHHmm}'"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME}_${YYYMMDDHHmm} < ${SITEDIR}/prod_tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
+# can either rsync including wp-config.php and sed it after, or just copy the old web root wp-config (this seds old file to a new file)
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "sed 's/.*DB_NAME.*/define(\x27DB_NAME\x27, \x27${DBNAME}_${YYYYMMDDHHmm}\x27);/' ${SITEDIR}/www/wp-config.php > ${SITEDIR}/www_new/wp-config.php"
+
 # next step, allow wordpress search-replace to happen
 # final task is to replace old with new
 ## mv www to www_old, www_new to www
--- rsync-source-to-target.sh
+++ rsync-source-to-target.sh
@@ -4,6 +4,5 @@
 export SOURCEIP=$(cat reseed-direction/source_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 export TARGETIP=$(cat reseed-direction/target_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${SOURCEIP} "rsync -e 'ssh -o StrictHostKeyChecking=no -i .ssh/deployer' --rsync-path='sudo rsync' --chown=${WEBUSER}:${WEBUSER} --chmod=D2775,F664 -a --progress --numeric-ids --delete ${SITEDIR}/*.sql deployer@${TARGETIP}:${SITEDIR}/"
+# exclude wp-config because it will be seeded in next task
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${SOURCEIP} "rsync -e 'ssh -o StrictHostKeyChecking=no -i .ssh/deployer' --rsync-path='sudo rsync' --chown=${WEBUSER}:${WEBUSER} --chmod=D2775,F664 -a --recursive --progress --numeric-ids --delete --exclude=wp-config.php ${WEBROOT}/ deployer@${TARGETIP}:${WEBROOT}_new/"
-# can either rsync including wp-config.php and sed it after, or just copy the old web root wp-config
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "cp ${WEBROOT}/wp-config.php ${WEBROOT}_new/wp-config.php"
