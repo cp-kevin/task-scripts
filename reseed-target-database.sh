52cfccce8900c37aa137fe1881b10c66e4f06a7a
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 16:13:50 -0700

    Update for reseed strategy

--- reseed-target-database.sh
+++ reseed-target-database.sh
@@ -8,9 +8,4 @@ rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progre
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'CREATE DATABASE ${DBNAME}_${YYYYMMDDHHmm}'"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME}_${YYYYMMDDHHmm} < ${SITEDIR}/prod_tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
 # can either rsync including wp-config.php and sed it after, or just copy the old web root wp-config (this seds old file to a new file)
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "sed 's/.*DB_NAME.*/define(\x27DB_NAME\x27, \x27${DBNAME}_${YYYYMMDDHHmm}\x27);/' ${SITEDIR}/www/wp-config.php > ${SITEDIR}/www_new/wp-config.php"
-
-# next step, allow wordpress search-replace to happen
-# final task is to replace old with new
-## mv www to www_old, www_new to www
-## drop old database (name will need to be grepped somehow. probably from wp-config.php of the old one)
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "sed 's/.*DB_NAME.*/define(\x27DB_NAME\x27, \x27${DBNAME}_${YYYYMMDDHHmm}\x27);/' ${WEBROOT}/wp-config.php > ${WEBROOT}_new/wp-config.php"
--- wordpress-search-replace.sh
+++ wordpress-search-replace.sh
@@ -6,9 +6,14 @@ export SOURCEURL=$(cat site-urls/source_site_url)
 export TARGETURL=$(cat site-urls/target_site_url)
 export SOURCEURLREGEX=$(cat site-urls/source_site_url_regex)
 export TARGETAADSSO=$(cat reseed-direction/target_aadsso | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${SOURCEURL} search-replace --regex '^${SOURCEURLREGEX}$' ${TARGETURL}"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} search-replace https://${SOURCEURL} https://${TARGETURL}"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} search-replace http://${SOURCEURL} http://${TARGETURL}"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} --skip-themes --skip-plugins option delete aadsso_settings"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${SOURCEURL} search-replace --regex '^${SOURCEURLREGEX}$' ${TARGETURL}"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} search-replace https://${SOURCEURL} https://${TARGETURL}"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} search-replace http://${SOURCEURL} http://${TARGETURL}"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} --skip-themes --skip-plugins option delete aadsso_settings"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "echo ${TARGETAADSSO} | base64 -d > target-aadsso"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT} --url=${TARGETURL} --skip-themes --skip-plugins option add aadsso_settings < target-aadsso; rm target-aadsso"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} --skip-themes --skip-plugins option add aadsso_settings < target-aadsso; rm target-aadsso"
+
+# next step, allow wordpress search-replace to happen
+# final task is to replace old with new
+## mv www to www_old, www_new to www
+## drop old database (name will need to be grepped somehow. probably from wp-config.php of the old one)
