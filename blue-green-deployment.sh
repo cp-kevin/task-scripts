735529a78ba6327ddcf6ca0420efe9df04d5c0f0
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 17:40:30 -0700

    Directory cleanup

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -8,3 +8,4 @@ ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "grep DB_NAME ${WEBROOT}_old/wp-config.php | cut -d \' -f4 > OLDDBNAME"
 # Use of single quotes here is intentional to allow the use of the target's variables
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'export OLDDBNAME=$(cat OLDDBNAME); mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE ${OLDDBNAME};"; rm /home/deployer/OLDDBNAME; rm /home/deployer/mysql_creds.cnf'
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "rm -r ${WEBROOT}_old"
