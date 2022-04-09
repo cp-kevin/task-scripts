5f9493774b70344c8f0927903322a39805837f6b
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 17:31:03 -0700

    Remove comments and enabled the rest of the script again

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# final task is to replace old with new
-## mv www to www_old, www_new to www
-## drop old database (name will need to be grepped somehow. probably from wp-config.php of the old one)
-
 export TARGETIP=$(cat reseed-direction/target_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progress target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
-# ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT} ${WEBROOT}_old"
-# ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT}_new ${WEBROOT}"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT} ${WEBROOT}_old"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT}_new ${WEBROOT}"
 # Store variable in file because of the delimiter causing issues with the next command
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "grep DB_NAME ${WEBROOT}_old/wp-config.php | cut -d \' -f4 > OLDDBNAME"
 # Use of single quotes here is intentional to allow the use of the target's variables
