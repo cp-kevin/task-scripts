3aad52a7d36aeacf444700d38468f68dca97469e
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 17:25:04 -0700

    Fix variable assignment

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -11,4 +11,4 @@ rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progre
 # Store variable in file because of the delimiter causing issues with the next command
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "grep DB_NAME ${WEBROOT}_old/wp-config.php | cut -d \' -f4 > OLDDBNAME"
 # Use of single quotes here is intentional to allow the use of the target's variables
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'export $(cat OLDDBNAME); mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE ${OLDDBNAME};"; rm /home/deployer/OLDDBNAME; rm /home/deployer/mysql_creds.cnf'
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'export OLDDBNAME=$(cat OLDDBNAME); mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE ${OLDDBNAME};"; rm /home/deployer/OLDDBNAME; rm /home/deployer/mysql_creds.cnf'
