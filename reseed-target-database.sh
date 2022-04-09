58e3bc67ab94cc0acd60e9b495e2ee29a0e1c5b0
Kevin Chee <kevin@craftypenguins.net>
2022-04-08 17:42:12 -0700

    Change file names

--- reseed-target-database.sh
+++ reseed-target-database.sh
@@ -6,6 +6,6 @@ rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progre
 # User has wildcard grant
 # GRANT ALL PRIVILEGES ON `redacted%`.* TO redacted@'127.0.0.1';
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'CREATE DATABASE ${DBNAME}_${YYYYMMDDHHmm}'"
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME}_${YYYYMMDDHHmm} < ${SITEDIR}/prod_tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 ${DBNAME}_${YYYYMMDDHHmm} < ${SITEDIR}/tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
 # can either rsync including wp-config.php and sed it after, or just copy the old web root wp-config (this seds old file to a new file)
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "sed 's/.*DB_NAME.*/define(\x27DB_NAME\x27, \x27${DBNAME}_${YYYYMMDDHHmm}\x27);/' ${WEBROOT}/wp-config.php > ${WEBROOT}_new/wp-config.php"
