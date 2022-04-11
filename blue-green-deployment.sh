6f0256d7880ee75c3c9ab20e7adf40bceac387a0
Kevin Chee <kevin@craftypenguins.net>
2022-04-11 15:23:04 -0700

    Add variable import/export for old db name

--- blue-green-deployment.sh
+++ blue-green-deployment.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 export TARGETIP=$(cat reseed-direction/target_ip | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
+export OLDDBNAME=$(cat www-old-info/old_db_name)
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" --progress target-webuser-mysql-creds/creds.cnf deployer@${TARGETIP}:/home/deployer/mysql_creds.cnf
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT} ${WEBROOT}_old"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mv ${WEBROOT}_new ${WEBROOT}"
-# Store variable in file because of the delimiter causing issues with the next command
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "grep DB_NAME ${WEBROOT}_old/wp-config.php | cut -d \' -f4 > OLDDBNAME"
-# Use of single quotes here is intentional to allow the use of the target's variables
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} 'export OLDDBNAME=$(cat OLDDBNAME); mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e "DROP DATABASE ${OLDDBNAME};"; rm /home/deployer/OLDDBNAME; rm /home/deployer/mysql_creds.cnf'
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "mysql --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 -e 'DROP DATABASE ${OLDDBNAME};'; rm /home/deployer/mysql_creds.cnf"
 ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "rm -r ${WEBROOT}_old"
--- dump-source-db.sh
+++ dump-source-db.sh
@@ -2,5 +2,9 @@
 
 export SOURCEIP=$(cat reseed-direction/source_ip  | openssl enc -d -aes-256-ctr -pbkdf2 -a -base64 -pass pass:${ENCRYPTPASS})
 rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" source-webuser-mysql-creds/creds.cnf deployer@${SOURCEIP}:/home/deployer/mysql_creds.cnf
+rsync -e "ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer" deployer@${SOURCEIP}:${WEBROOT}/wp-config.php ./
+grep DB_NAME wp-config.php | cut -d \' -f4 > www-old-info/old_db_name
+export OLDDBNAME=$(cat www-old-info/old_db_name)
+
 # compress would be improvement
-ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${SOURCEIP} "mysqldump --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 --single-transaction --no-create-db ${DBNAME} > ${SITEDIR}/tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
+ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${SOURCEIP} "mysqldump --defaults-extra-file=/home/deployer/mysql_creds.cnf -h 127.0.0.1 -P 6033 --single-transaction --no-create-db ${OLDDBNAME} > ${SITEDIR}/tmp_db_dump.sql; rm /home/deployer/mysql_creds.cnf"
