ccfefb28f810c9a0553105b88af1b26f4ba2eba4
Kevin Chee <kevin@craftypenguins.net>
2022-04-15 21:13:25 -0700

    Bypass error state in concourse

--- wordpress-search-replace.sh
+++ wordpress-search-replace.sh
@@ -36,6 +36,6 @@ for SITE in $(cat site-urls/site_urls); do
     ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${SITE} search-replace --regex '^${SOURCEURLREGEX}$' ${TARGETURL}"
     ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} search-replace https://${SOURCEURL} https://${TARGETURL}"
     ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} search-replace http://${SOURCEURL} http://${TARGETURL}"
-    ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} elementor replace_urls https://${SOURCEURL} https://${TARGETURL}"
+    ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} elementor replace_urls https://${SOURCEURL} https://${TARGETURL}" || continue
     ssh -o StrictHostKeyChecking=no -i deployer-ssh-key/deployer deployer@${TARGETIP} "wp --path=${WEBROOT}_new --url=${TARGETURL} elementor replace_urls http://${SOURCEURL} http://${TARGETURL}"
 done
